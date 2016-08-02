2a0efa28895a1e833e889667cdb19bac71c0f898
Pat Gavlin <pagavlin@microsoft.com>
2016-08-01 19:46:39 -0700

    Move the LIR NYI.

--- src/jit/lower.cpp
+++ src/jit/lower.cpp
@@ -3609,8 +3609,6 @@ void Lowering::DoPhase()
     }
     comp->EndPhase(PHASE_LOWERING_DECOMP);
 
-    NYI("later phases cannot yet process LIR!");
-
     comp->fgLocalVarLiveness();
     // local var liveness can delete code, which may create empty blocks
     if  (!comp->opts.MinOpts() && !comp->opts.compDbgCode)
@@ -3631,6 +3629,8 @@ void Lowering::DoPhase()
         comp->fgDispBasicBlocks(true);
     }
 #endif
+
+    NYI("later phases cannot yet process LIR!");
 
     // The initialization code for the TreeNodeInfo map was initially part of a single full IR
     // traversal and it has been split because the order of traversal performed by fgWalkTreePost
